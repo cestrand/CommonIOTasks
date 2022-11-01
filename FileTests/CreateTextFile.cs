2731169860269e7551af06cab22d50dd50aec81c
Marcin Kolenda <marcinkolenda419@gmail.com>
2022-11-01 12:03:31 +0100

    test dedicated for File class

---
+++ FileTests/CreateTextFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CommonIOTasks.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonIOTasks.FileTests;
+
+[TestClass]
+public class CreateTextFile : FileCreationTest
+{
+    public CreateTextFile() : base("file1.txt") { }
+ 
+
+    [TestMethod]
+    public void TestCreation()
+    {
+        StreamWriter writer = File.CreateText(Path!);
+        writer.WriteLine("Hello world!");
+        writer.Close();
+    }
+
+    [TestMethod]
+    public void TestParallel()
+    {
+        StreamWriter writer = File.CreateText(Path!);
+        writer.WriteLine("Hello world!");
+        writer.Close();
+    }
+
+}
