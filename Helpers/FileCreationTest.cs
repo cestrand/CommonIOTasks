e8babe78aab8fbf0897fee6d78ee78428edecd9f
Marcin Kolenda <marcinkolenda419@gmail.com>
2022-11-01 12:02:25 +0100

    FileCreationTestHelper that ensures file not exist

---
+++ Helpers/FileCreationTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonIOTasks.Helpers;
+public abstract class FileCreationTest
+{
+    public string Path
+    {
+        get; set;
+    }
+
+    public FileCreationTest(string path)
+    {
+        Path = path;
+    }
+
+    [ClassInitialize]
+    public void EnsureFileNotExists()
+    {
+        var fileInfo = new FileInfo(Path);
+        if (fileInfo.Exists)
+        {
+            fileInfo.Delete();
+        }
+    }
+
+    [ClassCleanup]
+    public void DeleteFile()
+    {
+        var fileInfo = new FileInfo(Path);
+        if (fileInfo.Exists)
+        {
+            fileInfo.Delete();
+        }
+    }
+}
