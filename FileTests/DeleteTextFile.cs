fae2ac6db84717a31cd1398330e721833051265f
Marcin Kolenda <marcinkolenda419@gmail.com>
2022-11-04 23:18:08 +0100

    test File.Delete method

---
+++ FileTests/DeleteTextFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonIOTasks.Helpers;
+
+namespace CommonIOTasks.FileTests;
+
+[TestClass]
+public class DeleteTextFile : WithTextFile
+{
+    public DeleteTextFile() : base("todelete.txt") {}
+
+    [TestMethod]
+    public void TestDelete()
+    {
+        Assert.IsTrue(File.Exists(Path!));
+
+        File.Delete(Path!);
+
+        Assert.IsFalse(File.Exists(Path!));
+    }
+
+    [TestMethod]
+    public void TestDeleteParallel()
+    {
+        Assert.IsTrue(File.Exists(Path!));
+
+        File.Delete(Path!);
+
+        Assert.IsFalse(File.Exists(Path!));
+    }
+}
