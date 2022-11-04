7392f2a53ff0c0ce5c23f7b5b449d21587ad7b53
Marcin Kolenda <marcinkolenda419@gmail.com>
2022-11-04 23:17:01 +0100

    WithTextFile helper which creates file for test

---
+++ Helpers/WithTextFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonIOTasks.Helpers;
+public abstract class WithTextFile
+{
+    public string Path
+    {
+        get; set;
+    }
+
+    public WithTextFile(string path)
+    {
+        Path = path;
+    }
+
+    [TestInitialize]
+    public void CreateFile()
+    {
+        var fileInfo = new FileInfo(Path);
+        if (!fileInfo.Exists)
+        {
+            var streamWriter = fileInfo.CreateText();
+            streamWriter.Write(".");
+            streamWriter.Close();
+        }
+    }
+
+    [TestCleanup]
+    public void DeleteFile()
+    {
+        var fileInfo = new FileInfo(Path);
+        if (fileInfo.Exists)
+        {
+            fileInfo.Delete();
+        }
+    }
+}
