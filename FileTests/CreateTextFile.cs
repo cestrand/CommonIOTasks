d9dcacb0b190449a16138132b42b130ca8967b05
Marcin Kolenda <marcinkolenda419@gmail.com>
2022-11-04 23:15:53 +0100

    correct CreateTextFile assertions

--- FileTests/CreateTextFile.cs
+++ FileTests/CreateTextFile.cs
@@ -13,17 +13,25 @@ public class CreateTextFile : EnsureFileNotExists
     [TestMethod]
     public void TestCreation()
     {
+        Assert.IsFalse(File.Exists(Path!));
+
         StreamWriter writer = File.CreateText(Path!);
         writer.WriteLine("Hello world!");
         writer.Close();
+
+        Assert.IsTrue(File.Exists(Path!));
     }
 
     [TestMethod]
     public void TestParallel()
     {
+        Assert.IsFalse(File.Exists(Path!));
+
         StreamWriter writer = File.CreateText(Path!);
         writer.WriteLine("Hello world!");
         writer.Close();
+
+        Assert.IsTrue(File.Exists(Path!));
     }
 
 }
